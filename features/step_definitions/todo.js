82786acaee13ac7fbe798bf5783c58adc8c25811
jimCresswell <jim.cresswell@gmail.com>
2015-03-04 16:17:48 +0000

    Tidying.

--- features/step_definitions/todo.js
+++ features/step_definitions/todo.js
@@ -6,34 +6,39 @@ chai.use(chaiAsPromised);
 
 var expect = chai.expect;
 
-var expectedTodoTitle;
-
 module.exports = function() {
     this.Given(/^I am on the app homepage$/, function (done) {
-        browser.get('http://localhost:3000/app/index.html');
+
+        // TODO: Create a page object factory function and a homepage object convenience object.
+        var homepageUrl = 'http://localhost:3000/app/index.html';
+        var homepageTitle = 'AngularJS • TodoMVC';
+
+        browser.get(homepageUrl);
 
         // Async page loading.
         expect(browser.getTitle())
-            .to.eventually.equal('AngularJS • TodoMVC')
+            .to.eventually.equal(homepageTitle)
             .notify(done);
     });
 
     this.When(/^I add a todo called "([^"]*)"\.$/, function (todoTitle, done) {
+        var world = this;
         var newTodoEl = element(by.model('newTodo'));
 
-        expectedTodoTitle = todoTitle;
-        
+        world.expectedTodoTitle = todoTitle;
+
         newTodoEl.sendKeys(todoTitle);
         newTodoEl.sendKeys('\n');
         done();
     });
 
     this.Then(/^I should see it added to the todo list\.$/, function (done) {
+        var world = this;
 
         // Unlike a WebElement the ElementFinder object understands the Angular digest loop.
         // http://angular.github.io/protractor/#/api?view=ElementFinder
         element(by.repeater('todo in todos').row(0)).element(by.tagName('label')).getText().then(function(todoTitle) {
-            expect(todoTitle).to.equal(expectedTodoTitle);
+            expect(todoTitle).to.equal(world.expectedTodoTitle);
             done();     
         });
     });

da8a12e22a7b1cac884fe739263e9b499437aae1
jimCresswell <jim.cresswell@gmail.com>
2015-03-05 14:11:16 +0000

    Docs.

--- page-objects/home-page.js
+++ page-objects/home-page.js
@@ -1,32 +1,66 @@
-// Note that browser and element are globals defined by Potractor. 
-// Should page objects have state? Could this be a plain object and so avoid instantiation?
+/**
+ * A page object for the example app home page.
+ * 
+ * Note that browser and element are globals defined by Potractor.
+ */
+/* global browser, element */ 
 'use strict';
 
+
+// Should page objects have state? Could this be a plain object and so avoid instantiation?
 function HomePage() {};
 module.exports = HomePage;
 
-// Unlike a WebElement the ElementFinder object understands the Angular digest loop.
-// http://angular.github.io/protractor/#/api?view=ElementFinder
+
+/**
+ * Elements on the page.
+ * 
+ * Unlike a WebElement the ElementFinder object understands the Angular digest loop.
+ * http://angular.github.io/protractor/#/api?view=ElementFinder
+ */
 var newTodoEl = element(by.model('newTodo'));
 var firstTodo = element(by.repeater('todo in todos').row(0));
 
+
+/**
+ * Constants.
+ */
 var url = 'http://localhost:3000/app/index.html';
 
 
+/**
+ * Navigate to the home page.
+ * @return {undefined}
+ */
 HomePage.prototype.get = function() {
   browser.get(url);
 };
 
+
+/**
+ * Get the page title.
+ * @return {string} page title.
+ */
 HomePage.prototype.getTitle = function() {
   return browser.getTitle();
 };
 
+
+/**
+ * Create a new todo item.
+ * @param  {string} todoText
+ * @return {undefined}
+ */
 HomePage.prototype.createTodo = function(todoText) {
   newTodoEl.sendKeys(todoText);
   newTodoEl.sendKeys('\n');
 };
 
-// Returns a promise.
+
+/**
+ * Get a promise for the text content of the first todo item.
+ * @return {promise} The text content of the first todo item.
+ */
 HomePage.prototype.getFirstTodoText = function() {
   return firstTodo.element(by.tagName('label')).getText();
 };

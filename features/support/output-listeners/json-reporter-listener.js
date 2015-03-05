4d2b7ea4e4e68ebd452ebca821ebe9e64943977e
jimCresswell <jim.cresswell@gmail.com>
2015-03-05 11:23:25 +0000

    Docs.

--- features/support/output-listeners/json-reporter-listener.js
+++ features/support/output-listeners/json-reporter-listener.js
@@ -1,8 +1,11 @@
+// This json output may allow Jenkins HTML reports to be generated with the Jenkins Cucumber Test Result plugin.
+// https://wiki.jenkins-ci.org/display/JENKINS/Cucumber+Test+Result+Plugin
 module.exports = function JsonOutputHook() {
   var Cucumber = require('cucumber');
   var JsonFormatter = Cucumber.Listener.JsonFormatter();
   var fs = require('fs');
 
+  // If this was being run in Jenkins this filepath could be injected as an environment variable in the Jenkins job.
   var filepath = 'cucumber-report.json';
 
   JsonFormatter.log = function (json) {

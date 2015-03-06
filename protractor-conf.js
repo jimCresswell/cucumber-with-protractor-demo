2ddb07bf235da81ddb4ecc73b47d14c4bff8a495
jimCresswell <jim.cresswell@gmail.com>
2015-03-06 11:08:18 +0000

    Docs.

--- protractor-conf.js
+++ protractor-conf.js
@@ -7,7 +7,11 @@ exports.config = {
   },
   specs: ['features/*.feature'],
 
-  // See cucumberOpts in https://github.com/angular/protractor/blob/master/docs/referenceConf.js 
+  // See cucumberOpts in https://github.com/angular/protractor/blob/master/docs/referenceConf.js
+  // A list of tags to run can be specified here e.g.
+  // tags: '@smoke'
+  // tags: ['@smoke', '@otherTag, @thirdTag']
+  // The Cucumber require path can be set with the 'require' property.
   cucumberOpts: {
     format: 'pretty'
   }
